1b9b1343107e72b52c2c55c48c766f6d9bc6201d
Lance <lanceatoledo@gmail.com>
2021-07-11 16:29:20 -0400

    removed logged in and fixed routes

--- src/App.js
+++ src/App.js
@@ -3,71 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
-import UserStore from './stores/UserStore';
 
 class App extends React.Component{
-
-  async componentDidMount(){
-
-    try{
-
-      // When app component mounts it will check if the user is currently logged in//
-      let res = await fetch('/isLoggedIn', {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-type': 'application/json'
-        }
-      });
-
-      let result = await res.json();
-
-      if (result && result.success){
-        UserStore.loading = false;
-        UserStore.isLoggedIn = true;
-        UserStore.username = result.username;
-      }
-
-      else {
-        UserStore.loading = false;
-        UserStore.isLoggedIn = false;
-      }
-
-    }
-  //Cache error handling//
-    catch(e) {
-      UserStore.loading = false;
-      UserStore.isLoggedIn = false;
-    }
-  }
-  
- //Logout Function//
-  async doLogout(){
-
-    try{
-
-      // When app component mounts it will check if the user is currently logged in//
-      let res = await fetch('/logout', {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-type': 'application/json'
-        }
-      });
-
-      let result = await res.json();
-      //Logsout the user and resets the username in the user stored
-      if (result && result.success){
-        UserStore.isLoggedIn = false;
-        UserStore.username = '';
-      }
-
-    }
-  //Cache error handling//
-    catch(e) {
-      console.log(e)
-    }
-  }
 
   render (){
     
@@ -103,9 +40,10 @@ class App extends React.Component{
     //   }
     // }
     return(
+      
     <Router>
       <Switch>
-        <Route path='/' component={Home} exact />
+        <Route path='/Autoholics' component={Home} exact />
         <Route path='/signin' component={SigninPage} exact />
       </Switch>
     </Router>
